12776e7c17f08c5d342753902ca643588dd8826d
ankitrajiit <rajankit2302@gmail.com>
2025-05-07 20:39:42 +0530

    Update streamlit_app_pending_order.py

--- streamlit_app_pending_order.py
+++ streamlit_app_pending_order.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+#from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col,when_matched
 
 
@@ -10,9 +10,9 @@ st.write(
     """
 )
 
+cnx=st.connection("snowflake")
 
-
-session = get_active_session()
+session = cnx.session()
 my_dataframe = session.table("smoothies.public.orders").filter(col("ORDER_FILLED")==0).collect()
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
@@ -43,10 +43,3 @@ else:
 
             
             
-
-
-
-
-
-
-
